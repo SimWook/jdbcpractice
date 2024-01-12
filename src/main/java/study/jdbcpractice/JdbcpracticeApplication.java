affafe4d674df9343e31a35efc8933752cb67a59
シム ウク <astralkaiser89@gmail.com>
2024-01-12 16:45:41 +0900

    Initial commit

---
+++ src/main/java/study/jdbcpractice/JdbcpracticeApplication.java
@@ -0,0 +1,13 @@
+package study.jdbcpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcpracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcpracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/jdbcpractice/JdbcpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package study.jdbcpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcpracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
